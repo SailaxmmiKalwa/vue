373af74083e1c89eaba5ef68d3092f471d369f26
SailaxmmiKalwa <147012779+SailaxmmiKalwa@users.noreply.github.com>
2023-12-06 17:52:03 -0700

    Add files via upload

--- src/components/topHero.vue
+++ src/components/topHero.vue
@@ -1,12 +1,11 @@
 <template>
     <div class="container">
-        <h1>Top 5 Companies and their revenues</h1>
+        <h1>Top 5 Companies by their revenues</h1>
     </div>
 </template>
 <style scoped>
 .container{
-    background-image: url('https://www.commercialcafe.com/blog/wp-content/uploads/sites/10/2020/09/most-popular-office-buildings-featured.jpg');
-    height: 400px;
+    height: 200px;
     width: auto;
     background-size: cover;
     background-position: center;
